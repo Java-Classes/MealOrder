c6f1636607ba30bf60b1d9a0e55f872a9f38a83c
Paul <paulageyev@gmail.com>
2018-04-05 15:35:15 +0300

    Refactor `Utils.js` as exported

--- client/user/src/mealorder/shell/utils/Utils.js
+++ client/user/src/mealorder/shell/utils/Utils.js
@@ -18,7 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*exported*/
+/*exported Utils */
 class Utils {
     static compareObjects(o1, o2) {
         for (let p in o1) {
