ff476916c71c33728feb413cbdde67cfa0f4be1e
Paul <paulageyev@gmail.com>
2018-04-06 13:07:25 +0300

    Refactor Utils as global for reducer

--- client/user/src/mealorder/shell/utils/Utils.js
+++ client/user/src/mealorder/shell/utils/Utils.js
@@ -18,7 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*global Utils */
+/*exported Utils */
 class Utils {
     static compareObjects(o1, o2) {
         for (let p in o1) {
