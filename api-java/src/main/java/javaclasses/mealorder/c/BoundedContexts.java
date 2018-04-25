997cd24eff29fe71e0e960fa47ca203844c0cd64
alexander.karpetc <alexander.karpetc@gmail.com>
2018-04-25 13:57:36 +0300

    Delete `Enricher` from `BoundedContext`

--- api-java/src/main/java/javaclasses/mealorder/c/BoundedContexts.java
+++ api-java/src/main/java/javaclasses/mealorder/c/BoundedContexts.java
@@ -21,8 +21,6 @@
 package javaclasses.mealorder.c;
 
 import io.spine.server.BoundedContext;
-import io.spine.server.event.EventBus;
-import io.spine.server.event.EventEnricher;
 import io.spine.server.storage.StorageFactory;
 import io.spine.server.storage.memory.InMemoryStorageFactory;
 import javaclasses.mealorder.c.order.OrderRepository;
@@ -99,17 +97,5 @@ public final class BoundedContexts {
                              .setStorageFactorySupplier(() -> storageFactory)
                              .setName(NAME)
                              .build();
-    }
-
-    private static EventBus.Builder createEventBus(StorageFactory storageFactory,
-                                                   PurchaseOrderRepository poRepository) {
-        final EventEnricher enricher = MealOrderEnrichments.newBuilder()
-                                                           .setPurchaseOrderRepository(poRepository)
-                                                           .build()
-                                                           .createEnricher();
-        final EventBus.Builder eventBus = EventBus.newBuilder()
-                                                  .setEnricher(enricher)
-                                                  .setStorageFactory(storageFactory);
-        return eventBus;
     }
 }
